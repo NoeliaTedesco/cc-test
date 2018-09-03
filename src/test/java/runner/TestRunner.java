1e1b88bb6ede76f361f781a29f30f1df84ce0a2b
NoeliaTedesco <33137554+NoeliaTedesco@users.noreply.github.com>
2018-09-03 16:06:12 -0300

    Se actualiza clase TestRunner

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
  */
 
 //@RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/java/features"} , format = {"json:target/cucumber.json","html:target/site/cucumber-pretty"},
+@CucumberOptions(features = {"src/test/java/feature"} , format = {"json:target/cucumber.json","html:target/site/cucumber-pretty"},
         glue = "steps")
 public class TestRunner extends AbstractTestNGCucumberTests{
 
